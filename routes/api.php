2eb5f0230e8fc297d9544a53d321667b24b08dd4
clintonkenedy <clintonventuracondori@gmail.com>
2022-07-03 04:10:50 -0500

    Login a la api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PagoController as PagoV1;
 use App\Http\Controllers\Api\V2\PagoController as PagoV2;
+use App\Http\Controllers\Api\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 */
 
-Route::apiResource('v1/pagos',PagoV1::class);
-Route::apiResource('v2/pagos',PagoV2::class);
+Route::apiResource('v1/pagos',PagoV1::class)
+    ->middleware('auth:sanctum');
+Route::apiResource('v2/pagos',PagoV2::class)
+    ->middleware('auth:sanctum');
+Route::post('login',[LoginController::class,'login']);
 
